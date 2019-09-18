e46ddb385b7065c5212e63324bede0b3905622ba
Phyllis Kan <pyk9q7@gmail.com>
2019-09-18 15:39:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,9 @@
 class Dog
+<<<<<<< HEAD
   def initialize(name, breed = "Mutt")
+=======
+  def initialize(name, breed)
+>>>>>>> a9966b49db916336245eced60e80986a1197e429
     @name = name
     @breed = breed
   end
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,14 @@
 class Person
+<<<<<<< HEAD
   def initialize (persons_name)
     @name = persons_name
   end
 end
+=======
+  def initialize(name)
+    @name = name
+  end
+end
+
+Dog.new("Mutt")
+>>>>>>> a9966b49db916336245eced60e80986a1197e429
