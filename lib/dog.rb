c0abd958490d7aebe636c7d209f8c4690931733b
Phyllis Kan <pyk9q7@gmail.com>
2019-09-18 15:40:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,8 @@
 class Dog
-<<<<<<< HEAD
+
   def initialize(name, breed = "Mutt")
 =======
   def initialize(name, breed)
->>>>>>> a9966b49db916336245eced60e80986a1197e429
     @name = name
     @breed = breed
   end
